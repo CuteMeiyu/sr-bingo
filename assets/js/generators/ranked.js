d9c3f657e48e9d4007b5369389b0d53c7272ae5f
Wang Meiyu <151729943+CuteMeiyu@users.noreply.github.com>
2024-12-27 09:07:40 +0000

    add loop option for ranked generator

--- assets/js/generators/ranked.js
+++ assets/js/generators/ranked.js
@@ -1,40 +1,41 @@
 class RankedGenerator {
-    static isValid(matrix, row, col, num) {
+    static isValid(matrix, row, col, num, loop) {
         if (matrix[row].includes(num)) return false;
         for (let i = 0; i < matrix.length; i++) {
             if (matrix[i][col] === num) return false;
         }
-        if (row === col) {
+        let size = matrix.length;
+        if (row === col || loop) {
             for (let i = 0; i < matrix.length; i++) {
-                if (matrix[i][i] === num) return false;
+                if (matrix[(i + row) % size][(i + col) % size] === num) return false;
             }
         }
-        if (row + col === matrix.length - 1) {
+        if (row + col === matrix.length - 1 || loop) {
             for (let i = 0; i < matrix.length; i++) {
-                if (matrix[i][matrix.length - 1 - i] === num) return false;
+                if (matrix[(i + row) % size][(size - i + col) % size] === num) return false;
             }
         }
         return true;
     }
 
-    static solve(matrix, row, col) {
+    static solve(matrix, row, col, loop) {
         const n = matrix.length;
         if (row === n) return true;
-        if (col === n) return this.solve(matrix, row + 1, 0);
-        if (matrix[row][col] != -1) return this.solve(matrix, row, col + 1);
+        if (col === n) return this.solve(matrix, row + 1, 0, loop);
+        if (matrix[row][col] != -1) return this.solve(matrix, row, col + 1, loop);
         let nums = Array.from({ length: n }, (_, i) => i);
         shuffleArray(nums);
         for (let num of nums) {
-            if (this.isValid(matrix, row, col, num)) {
+            if (this.isValid(matrix, row, col, num, loop)) {
                 matrix[row][col] = num;
-                if (this.solve(matrix, row, col + 1)) return true;
+                if (this.solve(matrix, row, col + 1, loop)) return true;
                 matrix[row][col] = -1;
             }
         }
         return false;
     }
 
-    static generateMatrix(ranks, centerHardest) {
+    static generateMatrix(ranks, centerHardest, loop) {
         for (let i = ranks.length; i < 5; i++) {
             ranks.push(0);
         }
@@ -43,7 +44,7 @@ class RankedGenerator {
             rankMatrix[2][2] = argmax(ranks);
         }
 
-        if (!this.solve(rankMatrix, 0, 0)) {
+        if (!this.solve(rankMatrix, 0, 0, loop)) {
             throw new Error("生成难度卡片时找不到满足条件的解");
         }
         for (let row = 0; row < 5; row++) {
@@ -72,8 +73,9 @@ class RankedGenerator {
         let games = settings.games;
         let ranks = settings.ranks;
         let centerHardest = settings.center;
+        let loop = settings.loop;
 
-        let rankMatrix = this.generateMatrix(ranks, centerHardest);
+        let rankMatrix = this.generateMatrix(ranks, centerHardest, loop);
         let rankIndexes = this.getIndexes(rankMatrix);
         for (let indexes of rankIndexes) {
             if (indexes !== undefined) shuffleArray(indexes);
